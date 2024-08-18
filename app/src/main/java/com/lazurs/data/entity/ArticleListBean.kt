d1efc85a4d3b112f948680f6f8aa25fde8709066
lazurs <shuai.zhang@colorv.com>
2024-08-18 14:43:16 +0800

    update ui

--- app/src/main/java/com/lazurs/data/entity/ArticleListBean.kt
+++ app/src/main/java/com/lazurs/data/entity/ArticleListBean.kt
@@ -6,6 +6,6 @@ package com.lazurs.data.entity
  */
 data class ArticleResponseData(val data:ArticleListBean?=null)
 data class ArticleListBean(val curPage:Int,val datas:List<ArticleBean>)
-data class ArticleBean(val shareUser:String?,
+data class ArticleBean(val author:String?,
     val niceDate:String?,val chapterName:String?,
 val title:String?)
--- app/src/main/java/com/lazurs/ui/component/AppComponent.kt
+++ app/src/main/java/com/lazurs/ui/component/AppComponent.kt
@@ -3,12 +3,17 @@ package com.lazurs.ui.component
 import android.content.Context
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.Row
+
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -27,6 +32,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -35,6 +42,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import coil.size.Size
+import coil.transform.CircleCropTransformation
 import com.lazurs.R
 import com.lazurs.data.entity.ArticleResponseData
 import com.lazurs.ui.theme.Black
@@ -84,9 +95,11 @@ fun EmptyOrErrorPage() {
 
 @Composable
 fun ArticleListPage(listData: ArticleResponseData) {
-    LazyColumn(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.Gray)) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Gray)
+    ) {
         items(items = listData.data?.datas ?: emptyList()) { article ->
 
             Card(
@@ -105,8 +118,30 @@ fun ArticleListPage(listData: ArticleResponseData) {
                     .padding(10.dp)
 
             ) {
-                NormalTextComponent(textValue = article.chapterName ?: "")
-                NormalTextComponent(textValue = article.title ?: "")
+
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Column {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentHeight(), verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            UserIconComponent(url = "https://www.wanandroid.com/blogimgs/42da12d8-de56-4439-b40c-eab66c227a4b.png")
+                            NormalTextComponent(textValue = article.author ?: "")
+                        }
+                        DesTextComponent(textValue = article.title ?: "")
+                    }
+                    Box(
+                        modifier = Modifier
+                            .align(Alignment.BottomStart)
+                            .padding(
+                                start = 8.dp, bottom = 12.dp
+                            )
+                    ) {
+                        BottomTextComponent(textValue = article.chapterName ?: "")
+                    }
+                }
+
             }
 
         }
@@ -115,6 +150,27 @@ fun ArticleListPage(listData: ArticleResponseData) {
 
 @Composable
 fun NormalTextComponent(
+    textValue: String, textStyle: TextStyle = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Normal,
+        color = Color.Black
+    )
+) {
+    Text(
+        text = textValue,
+        modifier = Modifier
+            .wrapContentWidth()
+            .wrapContentHeight()
+            .padding(start = 10.dp, end = 8.dp),
+        style = textStyle,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis
+
+    )
+}
+
+@Composable
+fun DesTextComponent(
     textValue: String, textStyle: TextStyle = TextStyle(
         fontSize = 18.sp,
         fontWeight = FontWeight.Normal,
@@ -125,11 +181,48 @@ fun NormalTextComponent(
         text = textValue,
         modifier = Modifier
             .wrapContentWidth()
-            .padding(16.dp)
-            .wrapContentHeight(),
+            .wrapContentHeight()
+            .padding(start = 10.dp, end = 8.dp, top = 10.dp),
         style = textStyle,
         maxLines = 1,
         overflow = TextOverflow.Ellipsis
 
+    )
+}
+
+@Composable
+fun BottomTextComponent(
+    textValue: String, textStyle: TextStyle = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Normal,
+        color = Color.Black
+    )
+) {
+    Text(
+        text = textValue,
+        modifier = Modifier
+            .wrapContentWidth()
+            .wrapContentHeight()
+            .padding(start = 10.dp, end = 8.dp),
+        style = textStyle,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+
+        )
+}
+
+@Composable
+fun UserIconComponent(url: String) {
+    AsyncImage(
+        modifier = Modifier
+            .width(40.dp)
+            .height(
+                40.dp
+            )
+            .padding(start = 10.dp),
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(url).transformations(CircleCropTransformation())
+            .build(), contentDescription = "",
+        contentScale = ContentScale.Crop
     )
 }
